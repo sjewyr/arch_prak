89f5e222f3ecd3d5d5ff8fa65cd9f8ae368fcb75
Vladimir <kiryanov_vova_134@mail.ru>
2025-04-12 01:32:43 +0300

    da

--- startup.sh
+++ startup.sh
@@ -1,1 +1,1 @@
-poetry run python generate.py && uvicorn main:app --host 0.0.0.0 --port 10000 --reload
+sleep 15 && poetry run python generate.py && uvicorn main:app --host 0.0.0.0 --port 10000 --reload
