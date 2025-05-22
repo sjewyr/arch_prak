da0739c8d22842b5abfca0f3839c6ca507aef330
sjewyr <ganshin.2004@gmail.com>
2025-05-23 00:06:32 +0300

    feat: redis

--- sync/generate.py
+++ sync/generate.py
@@ -344,11 +344,18 @@ if __name__ == "__main__":
 #проверка на наличие плагинов
 #  curl http://localhost:8083/connectors/elasticsearch-sink/status | jq
 #  curl http://localhost:8083/connectors/debezium-postgres-connector/status | jq
+#  curl http://localhost:8083/connectors/redis/status | jq
 
 
 #загрузка
 # curl -X POST -H "Content-Type: application/json" \
 #  --data @connectors/debezium-config.json \
+#  http://localhost:8083/connectors
+
+
+#загрузка
+# curl -X POST -H "Content-Type: application/json" \
+#  --data @connectors/redis-config.json \
 #  http://localhost:8083/connectors
 
 # curl -X POST -H "Content-Type: application/json" \
